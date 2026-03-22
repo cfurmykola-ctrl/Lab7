2789562d074f50d9a127abf2fc413cabdfc19a53
cfurmykola-ctrl <c.fur.mykola@student.uzhnu.edu.ua>
2026-03-22 17:59:00 +0200

    Add files via upload

---
+++ Lab7q2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class BankTerminal
+{
+    public Action<int> OnMoneyWithdraw;
+
+    public void Withdraw(int amount)
+    {
+        Console.WriteLine($"Знято {amount} грн");
+        OnMoneyWithdraw?.Invoke(amount);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        BankTerminal terminal = new BankTerminal();
+
+        terminal.OnMoneyWithdraw += amount =>
+            Console.WriteLine($"Лог: знято {amount} грн");
+
+        terminal.Withdraw(100);
+
+        terminal.OnMoneyWithdraw = null;
+
+        terminal.Withdraw(200);
+
+        terminal.OnMoneyWithdraw?.Invoke(9999);
+    }
+}
