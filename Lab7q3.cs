819e3558fff74c9aea2c5972f6bc0cb81bc3ae9c
cfurmykola-ctrl <c.fur.mykola@student.uzhnu.edu.ua>
2026-03-22 17:59:38 +0200

    Add files via upload

---
+++ Lab7q3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Func<double, double> discountCalculator = null;
+
+        discountCalculator += price => price * 0.95;
+        discountCalculator += price => price * 0.90;
+        discountCalculator += price => price - 100;
+
+        Console.Write("Введіть початкову ціну: ");
+        double priceValue = Convert.ToDouble(Console.ReadLine());
+
+        foreach (Func<double, double> func in discountCalculator.GetInvocationList())
+        {
+            priceValue = func(priceValue);
+        }
+
+        Console.WriteLine($"Кінцева ціна після знижок: {priceValue}");
+    }
+}
